d0cb2229f536a4d4d2a695eada6a8e282b83948c
Robert Gildein <robert.gildein@canonical.com>
2021-09-08 15:26:15 +0200

    Add configuration options
     - add exporter-listen-port options
       web.listen-address=<bind_address>:<exporter-listen-port options>
     - add exporter-stats-groups options
       bind.stats-groups=<exporter-stats-groups>
     - add config-changed hook
     - add function to manage prometheus-bind-exporter service
     - other configuration should be provided by relation w/ designate-bind

--- src/charm.py
+++ src/charm.py
@@ -17,6 +17,9 @@ from ops.model import ActiveStatus, MaintenanceStatus
 
 logger = logging.getLogger(__name__)
 
+DEFAULT_LISTEN_PORT = 9119
+DEFAULT_STATS_GROUPS = "server,view,tasks"
+
 
 class PrometheusBindExporterOperatorCharm(CharmBase):
     """Charm the service."""
@@ -26,15 +29,37 @@ class PrometheusBindExporterOperatorCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
         self.framework.observe(self.on.install, self._on_install)
-        self._stored.set_default()
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self._stored.set_default(listen_port=DEFAULT_LISTEN_PORT,
+                                 stats_groups=DEFAULT_STATS_GROUPS,)
         self.unit.status = ActiveStatus("Unit is ready")
 
     def _on_install(self, _):
-        """installation hook that installs prometheus-bind-exporter daemon."""
+        """Installation hook that installs prometheus-bind-exporter daemon."""
         self.unit.status = MaintenanceStatus("Installing prometheus-bind-exporter")
         snap_file = self.model.resources.fetch("prometheus-bind-exporter")
         subprocess.check_call(["snap", "install", "--dangerous", snap_file])
+        self._manage_prometheus_bind_exporter_service()
         self.unit.status = ActiveStatus("Unit is ready")
+
+    def _on_config_changed(self, _):
+        """Config change hook."""
+        self.unit.status = MaintenanceStatus("prometheus-bind-exporter configuration")
+        self._stored.listen_port = self.config.get("exporter-listen-port")
+        self._stored.stats_groups = self.config.get("exporter-stats-groups")
+        self._manage_prometheus_bind_exporter_service()
+        self.unit.status = ActiveStatus("Unit is ready")
+
+    def _manage_prometheus_bind_exporter_service(self):
+        """Manage the prometheus-bind-exporter service."""
+        logger.debug("prometheus-bind-exporter configuration in progress")
+        private_address = self.model.get_binding("designate-bind").network.bind_address
+        subprocess.check_call([
+            "snap", "set", "prometheus-bind-exporter",
+            f"web.listen-address={private_address or ''}:{self._stored.listen_port}",
+            f"web.stats-groups={self._stored.stats_groups}"
+        ])
+        logger.info("prometheus-bind-exporter has been reconfigured")
 
 
 if __name__ == "__main__":
